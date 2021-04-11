e97e8cb4df1c33f7f84f2541ae1840ab2c5fe68a
Ricardo <ricardinhogiasson16@gmail.com>
2021-04-11 20:20:42 -0300

    Finalizando Projeto e consumindo Api

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-     <div className="App">
-       <Conversor moedaA="USD" moedaB="BRL"/>
-     </div>
+      <div className="App">
+        <h1>Conversor De Moedas</h1>
+        <div className="Linha">
+          <Conversor moedaA="USD" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="USD" />
+        </div>
+        <div className="Linha">
+          <Conversor moedaA="CAD" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="CAD" />
+        </div>
+        <div className="Linha">
+          <Conversor moedaA="EUR" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="EUR" />
+        </div>
+        <div className="Linha">
+          <Conversor moedaA="NOK" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="NOK" />
+        </div>
+      </div>
     );
   }
 }
--- src/Components/Conversor.js
+++ src/Components/Conversor.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './Conversor.css'
 
 export default class Conversor extends Component {
   constructor(props) {
@@ -13,26 +14,27 @@ export default class Conversor extends Component {
     this.setState = this.setState.bind(this);
   }
 
-  defineValor(moedaA, moedaB){
-    if(moedaA == "USD" || moedaA =="usd" && moedaB == "BRL" || moedaB =="brl"){
-        return 5.68;
-    }else if(moedaA == "brl" || moedaA =="BRL" && moedaB == "usd" || moedaB =="USD"){
-        return 0.18;
-    }
+  defineValor(moedaA, moedaB) {
+    //Consumindo Api
+    let de_para = `${moedaA}_${moedaB}`;
+    let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=y&apiKey=2a753d4e0a33e4e6281d`;
+
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        let cotacao = json[de_para].val;
+        let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
+
+        this.setState({ moedaB_valor });
+      });
   }
 
-  converter(){
+  converter() {
+    this.defineValor(this.props.moedaA, this.props.moedaB);
 
-    const valor =this.defineValor(this.props.moedaA, this.props.moedaB);
-    
-        
-   
-    console.log(this.moedaA_valor);
-    this.setState({moedaB_valor: parseFloat(this.state.moedaA_valor) * valor})
-
-    console.log(this.state.moedaB_valor)
-
-
+    console.log(this.state.moedaB_valor);
   }
 
   render() {
@@ -41,9 +43,12 @@ export default class Conversor extends Component {
         <h2>
           {this.props.moedaA} para {this.props.moedaB}
         </h2>
-        <input type="text" onChange={(event) => {
-            this.setState({moedaA_valor: event.target.value})
-        }}></input>
+        <input
+          type="text"
+          onChange={(event) => {
+            this.setState({ moedaA_valor: event.target.value });
+          }}
+        ></input>
         <input type="button" value="Converter" onClick={this.converter}></input>
         <h2>{this.state.moedaB_valor}</h2>
       </div>
